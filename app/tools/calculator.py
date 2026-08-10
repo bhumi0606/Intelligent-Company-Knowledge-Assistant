16ecd6ed2fc4e54b2fb4a83107f5b0428a568d5b
bhumi <bhumikprajapati007@gmail.com>
2026-08-10 15:50:09 +0530

    feat: add calculator tool

---
+++ app/tools/calculator.py
@@ -0,0 +1,35 @@
+
+def tool_calculator(expression: str):
+    try:
+        expression = expression.strip()
+
+        if '+' in expression:
+            a, b = expression.split('+')
+            return float(a) + float(b)
+
+        if '-' in expression:
+            a, b = expression.split('-')
+            return float(a) - float(b)
+
+        if '*' in expression:
+            a, b = expression.split('*')
+            return float(a) * float(b)
+
+        if '/' in expression:
+            a, b = expression.split('/')
+            if b == 0:
+                raise ValueError("Can not divide by zero")
+            return float(a) / float(b)
+
+        if '**' in expression:
+            a, b = expression.split('**')
+            return float(a) ** float(b)
+
+        if '%' in expression:
+            a, b = expression.split('%')
+            if b == 0:
+                raise ValueError("can not modulo by zero")
+            return float(a) % float(b)
+
+    except Exception as e:
+        raise ValueError(f"Invalid expression: {expression}")
